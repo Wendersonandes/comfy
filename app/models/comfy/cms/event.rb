f3eb049eed79374a02f8befb03b9540592ce668b
Wendersonandes <oldmaverick@gmail.com>
2017-11-24 15:00:04 -0200

    Improved events management

--- app/models/comfy/cms/event.rb
+++ app/models/comfy/cms/event.rb
@@ -11,13 +11,22 @@ class Comfy::Cms::Event < ActiveRecord::Base
 
   # -- Callbacks ------------------------------------------------------------
   before_create     :assign_position
+	after_save				:set_all_others_unfeatured
 
   # -- Scopes ---------------------------------------------------------------
   default_scope -> { order('comfy_cms_events.position') }
+  scope :published, -> { where(:is_published => true) }
+  scope :featured, -> { where(:is_featured => true) }
 
   def assign_position
     max = Comfy::Cms::Event.maximum(:position)
     self.position = max ? max + 1 : 0
   end
+
+	def set_all_others_unfeatured
+		if self.is_featured
+			Comfy::Cms::Event.where('id != ? and is_featured and site_id = ?', self.id, self.site_id).update_all("is_featured = 'false'")
+		end
+	end
 
 end
