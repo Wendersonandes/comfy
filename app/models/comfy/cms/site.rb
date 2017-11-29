96586b74ae51cb93f4caa44e995b8377e65227a5
Wendersonandes <oldmaverick@gmail.com>
2017-11-29 13:17:36 -0200

    Added gallery and image associations to site model

--- app/models/comfy/cms/site.rb
+++ app/models/comfy/cms/site.rb
@@ -9,9 +9,11 @@ class Comfy::Cms::Site < ActiveRecord::Base
     site.has_many :files
     site.has_many :slides
     site.has_many :events
+    site.has_many :galleries
     site.has_many :videos
     site.has_many :menus
     site.has_many :menu_items, :through => :menus
+    site.has_many :images, :through => :galleries
     site.has_many :categories
   end
 
