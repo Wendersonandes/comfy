a3bd2754561df02b0b46f420ec8dbcf9512bbf69
Wendersonandes <oldmaverick@gmail.com>
2017-12-06 20:10:48 -0200

    Added Trash link to sidebar

--- app/jobs/delete_job.rb
+++
@@ -1,8 +0,0 @@
-require 'sucker_punch'
-class DeleteJob
-  include SuckerPunch::Job
-
-  def perform(data)
-    Shrine::Attacher.delete(data)
-  end
-end
--- app/jobs/upload_job.rb
+++
@@ -1,8 +0,0 @@
-require 'sucker_punch'
-class UploadJob
-  include SuckerPunch::Job
-
-  def perform(data)
-    Shrine::Attacher.promote(data)
-  end
-end
